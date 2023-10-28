17ddf277c7f2648ff3cdbce49d8b3a76129f4502
EvePulido <epulido3@ucol.mx>
2023-10-28 09:58:50 -0600

    Segunda versión

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ if (edadUsuario >= 18) {
     document.write("¡Lo siento, no eres mayor de edad!");
 }
 
+// Solicitar al usuario 3 calificaciones con un formulario de 3 input y un botón de enviar y sacar el promedio del usuario, 
+// si es mayor a 8 mostrarle en el DOM ¡Felicidades, lo lograste!, si no ¡Sigue estudiando!
+
+// function promedio() {
+
+//     const calificacion1 = parseFloat(document.getElementById('calificacion1').value);
+//     const calificacion2 = parseFloat(document.getElementById('calificacion2').value);
+//     const calificacion3 = parseFloat(document.getElementById('calificacion3').value);
+
+//     const promedio = (calificacion1 + calificacion2 + calificacion3) / 3;
+
+//     const resultadoContenedor = document.getElementById('resultado');
+//     if (promedio <= 10) {
+//         if (promedio >= 8) {
+//             resultadoContenedor.textContent += '¡Felicidades, lo lograste!'; 
+//         } else {
+//             resultadoContenedor.textContent += '¡Lo siento, tienes ordinario!';
+//         }
+//     } else {
+//         resultadoContenedor.textContent = 'Por favor, ingresa calificaciones válidas.';
+//     }
+// }
+    
